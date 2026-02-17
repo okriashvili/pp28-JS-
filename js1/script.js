f9fc45e3e199cb73e6d55c924f21e9719ceec63e
mahoo <146084234+okriashvili@users.noreply.github.com>
2026-02-17 22:16:58 +0100

    commit JS task

---
+++ js1/script.js
@@ -0,0 +1,32 @@
+x = "hello world"
+console.log(x)
+console.log(typeof x)
+
+  
+
+var y = 5
+console.log(y)
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+// for (i in [0, 1, 2, 3, 4, 5]) {
+//     console.log(i)
+// }   
+
+
+printWord = input("Enter a word: ")
+
+console.log(printWord)
+
+
+
+
+
+
+
+
+
+
