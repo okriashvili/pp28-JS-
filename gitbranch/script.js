d699b2fdb28f93f331efa7d5df28a8a62d0ef138
mahoo <146084234+okriashvili@users.noreply.github.com>
2026-03-18 16:34:09 +0100

    test commit, create new branch and push main branch to git repository

---
+++ gitbranch/script.js
@@ -0,0 +1,26 @@
+function oddOrEven(num) {
+    evenNum = [];
+    oddNum = [];
+    // imporve out function and test if forEach will works
+    // num.forEach(v => {
+    //     if (v % 2 ==0) {
+    //         evenNum.push(v);
+    //     } else {
+    //         oddNum.push(v);
+    //     }
+    // });
+    // it worked
+    // comment and test if for loop does same thing 
+    for (let i=0; i<num.length; i++) {
+        if (num[i] % 2 ==0) {
+            evenNum.push(num[i]);
+        } else {
+            oddNum.push(num[i]);
+        }
+    }
+    console.log(evenNum);
+    console.log(oddNum);
+}
+
+numAray = [1, 23, 43, 16, 78, 23, 43, 15, 16, 75, 10, 101, 23, 76];
+oddOrEven(numAray);
